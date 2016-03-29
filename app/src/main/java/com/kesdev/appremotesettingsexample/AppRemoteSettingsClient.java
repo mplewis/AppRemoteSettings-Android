dc4fbfc6a4691838e67488d234c6d8f3ea8ca97c
Matthew Lewis <matt@mplewis.com>
2016-03-28 22:23:06 -0500

    app: add docstrings

--- app/src/main/java/com/kesdev/appremotesettingsexample/AppRemoteSettingsClient.java
+++ app/src/main/java/com/kesdev/appremotesettingsexample/AppRemoteSettingsClient.java
@@ -25,94 +25,67 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class AppRemoteSettingsClient {
 
-    public abstract static class Handler<T> {
-        public abstract void onResult(T t);
-    }
-
-    private static class JsonPostRequestHandler {
-        HttpsURLConnection connection;
-        String body;
-        Handler<String> handler;
-
-        public JsonPostRequestHandler(String url, JSONObject data, Handler<String> handler)
-                throws IOException {
-
-            connection = (HttpsURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            body = data.toString();
-            this.handler = handler;
-        }
-    }
-
-    private static class HttpPostTask extends AsyncTask<JsonPostRequestHandler, Void, String> {
-        JsonPostRequestHandler requestHandler;
-
-        @Override
-        protected String doInBackground(JsonPostRequestHandler... params) {
-            requestHandler = params[0];
-            BufferedReader reader = null;
-            BufferedWriter writer = null;
-
-            //noinspection TryFinallyCanBeTryWithResources
-            try {
-                HttpsURLConnection connection = requestHandler.connection;
-
-                OutputStream outputStream = connection.getOutputStream();
-                writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
-                writer.write(requestHandler.body);
-                writer.close();
-
-                InputStream inputStream;
-                int status = connection.getResponseCode();
-                if (status >= 400 && status <= 599) {
-                    // Error occurred - we have to check getErrorStream instead of getInputStream
-                    // http://stackoverflow.com/a/5379364/254187
-                    inputStream = new BufferedInputStream(connection.getErrorStream());
-                } else {
-                    inputStream = new BufferedInputStream(connection.getInputStream());
-                }
-
-                reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
-                }
-                String respBody = stringBuilder.toString();
-
-                if (connection.getResponseCode() != HttpsURLConnection.HTTP_OK) {
-                    Log.e(TAG, "Response code was not 200: got " + connection.getResponseCode());
-                    Log.e(TAG, "Response body:");
-                    Log.e(TAG, "    " + respBody);
-                    return null;
-                }
-
-                return respBody;
-
-            } catch (Exception e) {
-                Log.e(TAG, "Error fetching JSON");
-                e.printStackTrace();
-                return null;
-
-            } finally {
-                try {
-                    if (writer != null) writer.close();
-                    if (reader != null) reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            if (requestHandler.handler != null && s != null) requestHandler.handler.onResult(s);
-        }
-    }
-
     public static final String TAG = "AppRemoteSettingsClient";
 
+    /**
+     * Updates a SharedPreferences object with values from an AppRemoteSettings server. Any
+     * existing keys that collide with keys in the AppRemoteSettings data will be replaced with the
+     * values from AppRemoteSettings.
+     *
+     * This method should be called as soon as possible in app execution so your settings are
+     * up-to-date quickly. You should probably call it in your main Activity's onCreate method.
+     *
+     * This method will never block or throw an exception.
+     *
+     * @param context       The {@link Context} to use for creating an HTTP connection
+     * @param endpointAPIv1 The URL for the AppRemoteSettings server's v1 API endpoint
+     * @param preferences   The {@link SharedPreferences} object to be updated with
+     *                      AppRemoteSettings data
+     */
+    public static void updatePreferencesWithAppRemoteSettings(
+            Context context, String endpointAPIv1, final SharedPreferences preferences) {
+        updatePreferencesWithAppRemoteSettings(context, endpointAPIv1, preferences, null);
+    }
+
+    /**
+     * Updates a SharedPreferences object with values from an AppRemoteSettings server. Any
+     * existing keys that collide with keys in the AppRemoteSettings data will be replaced with the
+     * values from AppRemoteSettings.
+     *
+     * This method should be called as soon as possible in app execution so your settings are
+     * up-to-date quickly. You should probably call it in your main Activity's onCreate method.
+     *
+     * This method will never block or throw an exception.
+     *
+     * @param context       The {@link Context} to use for creating an HTTP connection
+     * @param endpointAPIv1 The URL for the AppRemoteSettings server's v1 API endpoint
+     * @param preferences   The {@link SharedPreferences} object to be updated with
+     *                      AppRemoteSettings data
+     * @param onSuccess     The {@link Handler} to be called with a Map of {key: type} pairs for the
+     *                      keys received from AppRemoteSettings. This is only called if the
+     *                      operation succeeds. If it fails, nothing happens.
+     */
+    public static void updatePreferencesWithAppRemoteSettings(
+            Context context, String endpointAPIv1, final SharedPreferences preferences,
+            final Handler<Map<String, String>> onSuccess) {
+
+        fetchRawJsonFromAppRemoteSettings(context, endpointAPIv1, new Handler<String>() {
+            @Override
+            public void onResult(String rawJson) {
+                Map<String, String> added = parseRawJsonIntoSharedPreferences(rawJson, preferences);
+                if (onSuccess != null) onSuccess.onResult(added);
+            }
+        });
+    }
+
+    /**
+     * Fetch raw JSON from AppRemoteSettings' V1 endpoint. If the request is successful, handler
+     * is called with the response body. If it fails, nothing happens.
+     *
+     * @param context       The {@link Context} to use for creating an HTTP connection
+     * @param endpointAPIv1 The URL for the AppRemoteSettings server's v1 API endpoint
+     * @param handler       The {@link Handler} to call with response body on success
+     */
     private static void fetchRawJsonFromAppRemoteSettings(
             Context context, String endpointAPIv1, Handler<String> handler) {
 
@@ -127,6 +100,15 @@ public class AppRemoteSettingsClient {
         }
     }
 
+    /**
+     * Parse the raw JSON from AppRemoteSettings' V1 endpoint into typed objects. Store them in
+     * a {@link SharedPreferences} object and commit them to disk.
+     *
+     * @param rawJson       The JSON body from AppRemoteSettings, annotated with types
+     * @param preferences   The {@link SharedPreferences} object to be updated using
+     *                      AppRemoteSettings data
+     * @return
+     */
     private static Map<String, String> parseRawJsonIntoSharedPreferences(
             String rawJson, SharedPreferences preferences) {
 
@@ -170,22 +152,107 @@ public class AppRemoteSettingsClient {
         return added;
     }
 
-    public static void updatePreferencesWithAppRemoteSettings(
-            Context context, String endpointAPIv1, final SharedPreferences preferences,
-            final Handler<Map<String, String>> onSuccess) {
-
-        fetchRawJsonFromAppRemoteSettings(context, endpointAPIv1, new Handler<String>() {
-            @Override
-            public void onResult(String rawJson) {
-                Map<String, String> added = parseRawJsonIntoSharedPreferences(rawJson, preferences);
-                if (onSuccess != null) onSuccess.onResult(added);
-            }
-        });
+    /**
+     * Acts as a callback to return results asynchronously to the caller of
+     * updatePreferencesWithAppRemoteSettings when SharedPreferences are successfully updated.
+     * @param <T> the type of result to return
+     */
+    public abstract static class Handler<T> {
+        public abstract void onResult(T t);
     }
 
-    public static void updatePreferencesWithAppRemoteSettings(
-            Context context, String endpointAPIv1, final SharedPreferences preferences) {
-        updatePreferencesWithAppRemoteSettings(context, endpointAPIv1, preferences, null);
+    /**
+     * Contains the data necessary to run an HTTP POST request with a JSON body.
+     */
+    private static class JsonPostRequestHandler {
+        HttpsURLConnection connection;
+        String body;
+        Handler<String> handler;
+
+        public JsonPostRequestHandler(String url, JSONObject data, Handler<String> handler)
+                throws IOException {
+
+            connection = (HttpsURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            body = data.toString();
+            this.handler = handler;
+        }
+    }
+
+    /**
+     * The task used to run an asynchronous HTTP POST action, validate the response, and do
+     * something with the response body.
+     */
+    private static class HttpPostTask extends AsyncTask<JsonPostRequestHandler, Void, String> {
+        JsonPostRequestHandler requestHandler;
+
+        @Override
+        protected String doInBackground(JsonPostRequestHandler... params) {
+            requestHandler = params[0];
+            BufferedReader reader = null;
+            BufferedWriter writer = null;
+
+            //noinspection TryFinallyCanBeTryWithResources
+            try {
+                HttpsURLConnection connection = requestHandler.connection;
+
+                // Write the output stream to the HTTP connection
+                OutputStream outputStream = connection.getOutputStream();
+                writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+                writer.write(requestHandler.body);
+                writer.close();
+
+                // Get the input stream
+                InputStream inputStream;
+                int status = connection.getResponseCode();
+                if (status >= 400 && status <= 599) {
+                    // Error occurred - we have to check getErrorStream instead of getInputStream
+                    // http://stackoverflow.com/a/5379364/254187
+                    inputStream = new BufferedInputStream(connection.getErrorStream());
+                } else {
+                    inputStream = new BufferedInputStream(connection.getInputStream());
+                }
+
+                // Read the input stream to a string
+                reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                String respBody = stringBuilder.toString();
+
+                // Verify the status code was 200 OK
+                if (connection.getResponseCode() != HttpsURLConnection.HTTP_OK) {
+                    Log.e(TAG, "Response code was not 200: got " + connection.getResponseCode());
+                    Log.e(TAG, "Response body:");
+                    Log.e(TAG, "    " + respBody);
+                    return null;
+                }
+
+                // Return the response body as a string
+                return respBody;
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error fetching JSON");
+                e.printStackTrace();
+                return null;
+
+            } finally {
+                try {
+                    if (writer != null) writer.close();
+                    if (reader != null) reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (requestHandler.handler != null && s != null) requestHandler.handler.onResult(s);
+        }
     }
 
 }
